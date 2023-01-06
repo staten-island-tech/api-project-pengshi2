b33657ee55f2a780f22181caeff46929f52e408d
pengshi2 <boogiesnoogawoot@gmail.com>
2023-01-06 10:14:34 -0500

    v

--- api-project/api-project/js/main.js
+++ api-project/api-project/js/main.js
@@ -1,4 +1,4 @@
-const genshin = "https://api.genshin.dev/";
+const genshin = "https://api.jikan.moe/v4/recommendations/anime";
 
 async function getData(genshin) {
   try {
@@ -7,7 +7,9 @@ async function getData(genshin) {
       throw new Error(response);
     } else {
       const data = await response.json();
-      document.getElementById("api-response").textContent = data.content;
+      document.getElementById("api-response").textContent =
+        data.data[0].entry[0].title;
+      console.log(data.data[0].entry[0].title);
       console.log("horosho");
     }
   } catch (error) {
