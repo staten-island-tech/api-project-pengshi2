0a6becaebfbd0c7aa2f05fad442c07f0d8bccf45
pengshi2 <pengshi0921@gmail.com>
2023-01-31 19:25:42 -0500

    b

--- api-project/api-project/js/api.js
+++ api-project/api-project/js/api.js
@@ -1,10 +1,5 @@
+import { DOM } from "./dom";
 const skins = "https://valorant-api.com/v1/bundles";
-
-const DOM = {
-  form: document.getElementById("#skinbar"),
-  name: document.getElementById("name"),
-  card: document.getElementsByClassName("card"),
-};
 
 async function getData(skins) {
   try {
@@ -13,6 +8,7 @@ async function getData(skins) {
       throw new Error(response);
     } else {
       const data = await response.json();
+      console.log(data);
       data.data.forEach((bundle) => {
         document.getElementById("display").insertAdjacentHTML(
           "beforeend",
@@ -32,4 +28,91 @@ async function getData(skins) {
     console.log("error");
   }
 }
+
 getData(skins);
+
+const displayDiv = document.querySelector("#display");
+
+document.querySelector("#all").addEventListener("click", function () {
+  displayDiv.innerHTML = ``;
+  apple();
+});
+
+document.querySelector("#best").addEventListener("click", function () {
+  displayDiv.innerHTML = ``;
+  orange();
+});
+
+document.querySelector("#random").addEventListener("click", function () {
+  displayDiv.innerHTML = ``;
+  pear();
+});
+async function bananna() {
+  const response = await fetch(skins);
+  const info = await response.json();
+  info.data
+    .filter((bundle) => bundle.displayName.includes("Spectrum"))
+    .forEach((bundle) =>
+      document.getElementById("display").insertAdjacentHTML(
+        "beforeend",
+        `<div class = "card">
+      <div id = "${bundle.displayName}">
+      <h2 class = "bundle"> ${bundle.displayName} </h2>
+      <img class = "bundle-images" src = ${bundle.displayIcon} alt = ""> 
+      </div>
+      </div>`
+      )
+    );
+}
+
+async function apple() {
+  const response = await fetch(skins);
+  const info = await response.json();
+  info.data.forEach((bundle) =>
+    document.getElementById("display").insertAdjacentHTML(
+      "beforeend",
+      `<div class = "card">
+      <div id = "${bundle.displayName}">
+      <h2 class = "bundle"> ${bundle.displayName} </h2>
+      <img class = "bundle-images" src = ${bundle.displayIcon} alt = ""> 
+      </div>
+      </div>`
+    )
+  );
+}
+
+async function pear() {
+  const response = await fetch(skins);
+  const info = await response.json();
+  const data = info.data;
+  const bundle = data[parseInt(Math.random() * data.length)];
+  document.getElementById("display").insertAdjacentHTML(
+    "beforeend",
+    `<div class = "card">
+    <div id = "${bundle.displayName}">
+      <h2 class = "bundle"> ${bundle.displayName} </h2>
+      <img class = "bundle-images" src = ${bundle.displayIcon} alt = ""> 
+      </div>
+      </div> `
+  );
+}
+
+async function orange() {
+  const response = await fetch(skins);
+  const info = await response.json();
+  const data = info.data;
+  const bundles = data
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 5)
+    .forEach((bundle) =>
+      document.getElementById("display").insertAdjacentHTML(
+        "beforeend",
+        `<div class = "card">
+  <div id = "${bundle.displayName}">
+    <h2 class = "bundle"> ${bundle.displayName} </h2>
+    <img class = "bundle-images" src = ${bundle.displayIcon} alt = ""> 
+    </div>
+    </div> `
+      )
+    );
+}
---
+++ api-project/api-project/js/dom.js
@@ -0,0 +1,6 @@
+export const DOM = {
+  display: document.getElementById("display"),
+  all: document.getElementById("all"),
+  best: document.getElementById("best"),
+  random: document.getElementById("random"),
+};
